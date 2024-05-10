d34b38053d76c1109abfba583b5b93cc5dd56eac
Aystb <3185269857@qq.com>
2024-05-10 17:33:08 +0800

    feat/棋盘基础点击功能完成

---
+++ work/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace work
+{
+    public  static class Utils
+    {
+        public static int getIndex(double inch,double pos) {
+          
+            int res = 1;
+            while (inch*res <=pos) {
+                res++;
+            }
+            return res-1;
+        }
+    }
+}
