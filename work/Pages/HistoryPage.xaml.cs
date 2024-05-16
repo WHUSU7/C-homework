9d39159e30e51233c44fb2fe205d8b128c071189
wyob <1660784652@qq.com>
2024-05-16 16:05:02 +0800

    历史页面更新

--- work/Pages/HistoryPage.xaml.cs
+++ work/Pages/HistoryPage.xaml.cs
@@ -12,30 +12,70 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace work.Pages
 {
     /// <summary>
     /// HistoryPage.xaml 的交互逻辑
     /// </summary>
-    public partial class HistoryPage : Page
+    public partial class HistoryPage : Page, INotifyPropertyChanged
     {
+        private int a;
         public HistoryPage()
         {
             InitializeComponent();
-            this.Loaded += HistoryPageLoaded;
+            DataContext = GameService.Instance; // 设置数据上下文为当前窗口实例
         }
 
-        private void HistoryPageLoaded(object sender, RoutedEventArgs e)
+        // INotifyPropertyChanged 接口的实现
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
         {
-            //Utils.PrintBoard(Board.getBoardInstance());
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-       
 
-        public void jumpBackToMain(object sender, RoutedEventArgs e) {
-
+        //页面跳转
+        public void jumpBackToMain(object sender, RoutedEventArgs e)
+        {
             MainWindow.window.jumpToTargetPage(MainWindow.WindowsID.main);
         }
     }
+
+
+    public class GameService
+    {
+        private static GameService _instance;
+        public ObservableCollection<string> Result { get; } = new ObservableCollection<string>();
+
+        private GameService() { }
+
+        public static GameService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameService();
+                }
+                return _instance;
+            }
+        }
+        int turn = 0;
+        public static string[] historyStep = new string[42];
+
+        public void getPosition(int x, int y)
+        {
+            // ... 你的逻辑 ...
+            historyStep[turn] = "{x},{y}";
+            turn++;
+            string moveDescription = (turn % 2 != 0)
+                ? $"白子落子位置为{x},{y}"
+                : $"黑子落子位置为{x},{y}";
+            Result.Add(moveDescription);
+        }
+    }
+
 }
