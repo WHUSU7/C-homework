371414337282ed85829eeed99ea021f1732ce302
hvenjustic <14156846+hvnjustic@user.noreply.gitee.com>
2024-05-22 18:31:15 +0800

    pvp新增落子倒计时

--- work/Pages/PVP.xaml.cs
+++ work/Pages/PVP.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace work.Pages
 {
@@ -22,26 +23,142 @@ namespace work.Pages
     /// </summary>
     public partial class PVP : Page
     {
+		//数据绑定
+		public class MainDataModel : INotifyPropertyChanged
+		{
+			private double _canvasWidth;
+			private string _matchText;
+			private string _backText;
+			private string _ourSide;
+			private string _oppSide;
+			public double CanvasWidth
+			{
+				get { return _canvasWidth; }
+				set
+				{
+					if (_canvasWidth != value)
+					{
+						_canvasWidth = value;
+						OnPropertyChanged(nameof(CanvasWidth));
+					}
+				}
+			}
+
+			public string MatchText
+			{
+				get { return _matchText; }
+				set
+				{
+					if (_matchText != value)
+					{
+						_matchText = value;
+						OnPropertyChanged(nameof(MatchText));
+					}
+				}
+			}
+
+			public string BackText
+			{
+				get { return _backText; }
+				set
+				{
+					if (_backText != value)
+					{
+						_backText = value;
+						OnPropertyChanged(nameof(BackText));
+					}
+				}
+			}
+			public string OurSide
+			{
+				get { return _ourSide; }
+				set
+				{
+					if (_ourSide != value)
+					{
+						_ourSide = value;
+						OnPropertyChanged(nameof(OurSide));
+					}
+				}
+			}
+			public string OppSide
+			{
+				get { return _oppSide; }
+				set
+				{
+					if (_oppSide != value)
+					{
+						_oppSide = value;
+						OnPropertyChanged(nameof(OppSide));
+					}
+				}
+			}
+			//元素改变时候触发的委托（监听）
+			public event PropertyChangedEventHandler PropertyChanged;
+
+			protected virtual void OnPropertyChanged(string propertyName)
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
 		MainDataModel mdm;
+		private DispatcherTimer timer;
+		private int timeLeft;
 		public PVP()
         {
             InitializeComponent();
 			mdm = new MainDataModel();
 			this.DataContext = mdm;
 			App.PVPInstance = this;
+			timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += Timer_Tick;
 		}
-        public void jumpBackToMain(object sender, RoutedEventArgs e)
+		//落子倒计时
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			if (timeLeft > 0)
+			{
+				timeLeft--;
+				mdm.OurSide = timeLeft.ToString();
+			}
+			else
+			{
+				timer.Stop();
+				
+			}
+		}
+		private void countDown1()
+		{
+			timeLeft = 9;
+			mdm.OurSide=timeLeft.ToString();
+			timer.Start();
+		}
+		private void countDown2()
+		{
+			timeLeft = 9;
+			mdm.OppSide = timeLeft.ToString();
+			timer.Start();
+		}
+		public void jumpBackToMain(object sender, RoutedEventArgs e)
         {
-
             MainWindow.window.jumpToTargetPage(MainWindow.WindowsID.main);
         }
 		//开始匹配，涉及网络通信，匹配成功后，按钮更改
         public void startToMatch(object sender, RoutedEventArgs e)
         {
+			mdm.MatchText = "匹配中";
+			mdm.BackText = "终止对局并返回主页";
+			//match();匹配函数
+			mdm.MatchText = "正在对局";
+			countDown1();
+			//交互函数
+			countDown2();
+		}
+		
 
-        }
 		//点击落子操作，与aixaml.cs逻辑类似
-        private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
 			Point clickPoint = e.GetPosition(myCanvas);
 
@@ -56,6 +173,7 @@ namespace work.Pages
 			int y = Utils.getIndex(buttonWidthSize, clickPoint.X);
 			string targetBtn = "Button" + x.ToString() + y.ToString();
 			Button btn = (Button)FindName(targetBtn);
+
 		}
 		//外观适应，无需更改
 		private void myCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -72,30 +190,6 @@ namespace work.Pages
 			Button btn = sender as Button;
 		}
 		//数据绑定
-		public class MainDataModel : INotifyPropertyChanged
-		{
-			private double _canvasWidth;
-
-			public double CanvasWidth
-			{
-				get { return _canvasWidth; }
-				set
-				{
-					if (_canvasWidth != value)
-					{
-						_canvasWidth = value;
-						OnPropertyChanged(nameof(CanvasWidth));
-					}
-				}
-			}
-
-			//元素改变时候触发的委托（监听）
-			public event PropertyChangedEventHandler PropertyChanged;
-
-			protected virtual void OnPropertyChanged(string propertyName)
-			{
-				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-			}
-		}
+		
 	}
 }
