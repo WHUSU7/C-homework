ee7de2d6819f78fe81c1a40244b86cdde97e2b8d
李岩 <2902299850@qq.com>
2024-05-25 23:55:18 +0800

    丰富主界面，添加人机随机延迟

--- work/Models/User.cs
+++ work/Models/User.cs
@@ -11,12 +11,14 @@ namespace work.Models
         public int id;
         public string name;
         public string password;
+        public string nickname;
 
-        public User(int id, string name, string password)
+        public User(int id, string name, string password, string nickname)
         {
             this.id = id;
             this.name = name;
             this.password = password;
+            this.nickname = nickname;
         }
     }
 }
